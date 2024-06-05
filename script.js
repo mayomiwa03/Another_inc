d2133816cea7623d191a5fb037f348604368e78d
Daniel Adeagbo <oluwamayomiwa03@gmail.com>
2024-06-06 00:25:45 +0100

    update

--- script.js
+++ script.js
@@ -8,7 +8,14 @@ const choose = function () {
 
 document.querySelector(".plan").addEventListener("click", choose);
 
-
 const yearEl = document.querySelector(".year");
 const currentYear = new Date().getFullYear();
- yearEl.textContent = currentYear;
+yearEl.textContent = currentYear;
+
+// make mobile navigation work
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
